f43e593f8cd7b75834cb47e175237879e1577584
tianze0926 <i@tianze.me>
2022-01-20 23:20:21 +0800

    add record history

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -1,8 +1,8 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
-const water_api = axios.create({
-  baseURL: '/water',
+const api = axios.create({
+  baseURL: '/api',
 })
 
 export default boot(({ app }) => {
@@ -12,9 +12,9 @@ export default boot(({ app }) => {
   // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
   //       so you won't necessarily have to import axios in each vue file
 
-  app.config.globalProperties.$api = water_api
+  app.config.globalProperties.$api = api
   // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
   //       so you can easily perform requests against your app's API
 })
 
-export { axios, water_api }
+export { axios, api }
--- models/record.go
+++ models/record.go
@@ -4,6 +4,6 @@ package models
 type Record struct {
 	ID         uint   `json:"id"`
 	Time       int    `json:"time" gorm:"autoCreateTime:milli"`
-	IP         string `json:"ip" binding:"required"`
+	IP         string `json:"ip"`
 	FileNumber string `json:"file_number" binding:"required"`
 }
